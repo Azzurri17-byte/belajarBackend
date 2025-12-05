83cd7acd0eee65d5c2fcf3ec7c7c955c9237f88f
Raihan Gambut <cukung@gmail.com>
2025-12-05 14:58:55 +0800

    main file

--- server.js
+++ server.js
@@ -12,23 +12,3 @@ app.use('/book', bookRouter)
 app.listen(port, () => {
     console.log(`listen in ${port}`)
 })
-
-
-
-
-
-
-// const express = require('express')
-// const app = express()
-// const bodyParser = require('body-parser')
-// const port = 3000
-// const authRouter = require('./routes/auth')
-// const bookRouter = require('./routes/book')
-
-// app.use(bodyParser.json())
-// app.use('/auth', authRouter)
-// app.use('/book', bookRouter)
-
-// app.listen(port, () => {
-//     console.log(`listen in port ${port}`)
-// })
