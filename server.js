77deaea343e8fe2b76478d8230c2c792ee582084
Raihan Gambut <cukung@gmail.com>
2025-12-05 14:57:46 +0800

    main file

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 3000
 const authRouter = require('./routes/auth')
 const bookRouter = require('./routes/book')
+const port = 3000
 
 app.use(bodyParser.json())
 app.use('/auth', authRouter)
 app.use('/book', bookRouter)
 
 app.listen(port, () => {
-    console.log(`listen in port ${port}`)
+    console.log(`listen in ${port}`)
 })
+
+
+
+
+
+
+// const express = require('express')
+// const app = express()
+// const bodyParser = require('body-parser')
+// const port = 3000
+// const authRouter = require('./routes/auth')
+// const bookRouter = require('./routes/book')
+
+// app.use(bodyParser.json())
+// app.use('/auth', authRouter)
+// app.use('/book', bookRouter)
+
+// app.listen(port, () => {
+//     console.log(`listen in port ${port}`)
+// })
