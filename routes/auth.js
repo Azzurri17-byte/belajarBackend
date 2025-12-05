68d6aa4789140203ea45869f0accb5d24e523d5c
Raihan Gambut <cukung@gmail.com>
2025-12-05 14:24:40 +0800

    router authentication & authorization

--- routes/auth.js
+++ routes/auth.js
@@ -12,53 +12,48 @@ router.post('/register', async (req, res) => {
     const role = req.body.role || "user"
 
     if (!username || !password) {
-        return response(400, 'username & password harus diisi', '-', res)
+        return response(400, 'username & password harus diisi')
     }
-    //apabila akun sudah terdaftar
     db.query('SELECT * FROM users_1 WHERE username = ?', [username], (err, result) => {
         if (err) {
             return response(500, 'SERVER ERROR', '-', res)
         }
-        return response(409, 'username sudah terdaftar', `${result[0].username} sudah terdaftar`, res)
-    })
-    //proses register
+        return response(409, 'username sudah terdaftar coba lagi', result[0].username, res)
+        })
     const hashedPassword = await bcrypt.hash(password, 10)
     const sql = 'INSERT INTO users_1 (username, password, role) VALUES (?, ?, ?)'
     db.query(sql, [username, hashedPassword, role], (err, result) => {
         if (err) {
             return response(500, 'SERVER ERROR', '-', res)
         }
-        return response(201, 'Register Berhasil', result.insertId, res)
+        return response(201, 'Login berhasil', result.insertId, res)
     })
 })
 
-//proses login
 router.post('/login', (req, res) => {
     const {username, password} = req.body
-    if(!username || !password) {
+    if (!username || !password) {
         return response(400, 'username & password harus diisi')
     }
     const sql = 'SELECT * FROM users_1 WHERE username = ?'
     db.query(sql, [username], async (err, result) => {
         if (err) {
-            return response(500, 'SERVER ERROR', '-', res)
+            return response(500, "SERVER ERROR", '-', res)
         }
         if (result.length === 0) {
-            return response(404, 'username tidak terdaftar', '-', res)
+            return response(404, 'username tidak ditemukan', '-', res)
         }
         const user = result[0]
         const passwordMatch = bcrypt.compare(password, user.password)
         if (!passwordMatch) {
-            return response(400, 'password salah', '-', res)
+            return response(404, 'password salah', '-', res)
         }
         const token = jwt.sign({
             id: user.id,
             username: user.username,
-            role: user.role
-        },
-        secretkey, {expiresIn: "1h"}
-    )
-    return response(200, 'Login berhasil', {token}, res)
+            role: user.role, 
+        }, secretkey, {expiresIn: "1h"})
+        return response(200, 'Login berhasil', {token}, res)
     })
 })
 
