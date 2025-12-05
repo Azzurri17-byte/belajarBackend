bf31d897bf9a54486a7d8c01bf1a8f76fb7a94d5
Raihan Gambut <cukung@gmail.com>
2025-12-05 13:49:08 +0800

    sql

--- connection.js
+++ connection.js
@@ -9,8 +9,3 @@ const db = mysql.createConnection({
 
 module.exports = db
 
-/* 
-halo ini saya raihan saya disini hanya ingin mengetik sebuah kalimat, 
-daripada daya mmenggunakan lorom ipsum saya hanya ingin memberika sepatah kalimat, 
-jangan patah semangat dan terus berjuang okeee
-*/
