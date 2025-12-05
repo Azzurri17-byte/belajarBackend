78a2929850ce79240c9866807598fd418b5734f3
Raihan Gambut <cukung@gmail.com>
2025-12-05 14:45:46 +0800

    verify token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,16 +6,16 @@ const secretkey = process.env.SECRET_KEY
 const verifyToken = (roles = []) => {
     return (req, res, next) => {
         const authHeader = req.headers.authorization
-        if(!authHeader) {
-            return response(401, 'token tidak ditemukan', '-', res)
+        if (!authHeader) {
+            return response(401, 'Token tidak ditemukan', '-', res)
         }
         const token = authHeader.split(" ")[1]
         jwt.verify(token, secretkey, (err, decode) => {
             if (err) {
                 return response(401, 'token invalid/expired', '-', res)
             }
-            if (roles.length > 0 && !roles.includes(decode.role)) {
-                return response(403, 'akses ditolak', '-', res)
+            if(roles.length > 0 && !roles.includes(decode.role)) {
+                return response(403, 'Akses ditolak!!', '-', res)
             }
             req.user = decode
             next()
@@ -24,3 +24,35 @@ const verifyToken = (roles = []) => {
 }
 
 module.exports = verifyToken
+
+
+
+
+
+
+// require('dotenv').config()
+// const jwt = require('jsonwebtoken')
+// const response = require('../response')
+// const secretkey = process.env.SECRET_KEY
+
+// const verifyToken = (roles = []) => {
+//     return (req, res, next) => {
+//         const authHeader = req.headers.authorization
+//         if(!authHeader) {
+//             return response(401, 'token tidak ditemukan', '-', res)
+//         }
+//         const token = authHeader.split(" ")[1]
+//         jwt.verify(token, secretkey, (err, decode) => {
+//             if (err) {
+//                 return response(401, 'token invalid/expired', '-', res)
+//             }
+//             if (roles.length > 0 && !roles.includes(decode.role)) {
+//                 return response(403, 'akses ditolak', '-', res)
+//             }
+//             req.user = decode
+//             next()
+//         })
+//     }
+// }
+
+// module.exports = verifyToken
