dee34da94088fb79fc055e824a092ae60a28957f
Raihan Gambut <cukung@gmail.com>
2025-12-06 12:51:58 +0800

    sistem verify token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,22 +5,36 @@ const secretkey = process.env.SECRET_KEY
 
 const verifyToken = (roles = []) => {
     return (req, res, next) => {
+        //token dari cookie web
+        let token = req.cookies.token
+        
+    //kalau gaada token di cookie web, coba ambil di postman    
+    if (!token && req.headers.authorization) {
         const authHeader = req.headers.authorization
-        if (!authHeader) {
-            return response(401, 'Token tidak ditemukan', '-', res)
+        token = authHeader.split(" ")[1]
+    }
+
+    if (!token) {
+        //kalau request dari browser ambil pakai redirect
+        if (req.originalUrl.startWith("/auth")) {
+            return res.redirect('/auth/login')
         }
-        const token = authHeader.split(" ")[1]
-        jwt.verify(token, secretkey, (err, decode) => {
-            if (err) {
-                return response(401, 'token invalid/expired', '-', res)
-            }
-            if(roles.length > 0 && !roles.includes(decode.role)) {
+        //kalau request via postman kirim ini
+        return response(404, 'token tidak ditemukan', '-', res)
+    }
+
+    try {
+          const decoded = jwt.verify(token, secretkey)
+            if(roles.length > 0 && !roles.includes(decoded.role)) {
                 return response(403, 'Akses ditolak!!', '-', res)
             }
-            req.user = decode
+            req.user = decoded
             next()
-        })
+        } catch {
+            return response(401, "token invalid/expired", '-', res)
+        }
     }
 }
+
 
 module.exports = verifyToken
