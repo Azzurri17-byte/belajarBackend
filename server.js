a530cc320a40e89f9459329e23e669c85e25d044
Raihan Gambut <cukung@gmail.com>
2025-12-06 12:59:01 +0800

    main file

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const app = express()
 const bodyParser = require('body-parser')
 const authRouter = require('./routes/auth')
 const bookRouter = require('./routes/book')
+const cookieParser = require('cookie-parser')
 const port = 3000
 
+app.set('view engine', 'ejs')
 app.use(bodyParser.json())
+app.use(express.json())
+app.use(cookieParser())
 app.use('/auth', authRouter)
 app.use('/book', bookRouter)
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(port, () => {
     console.log(`listen in ${port}`)
