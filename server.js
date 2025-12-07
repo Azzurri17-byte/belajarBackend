be2232507fa33f5cdc286bed2babfbd171ad8812
Raihan Gambut <cukung@gmail.com>
2025-12-07 16:19:01 +0800

    main file

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bookRouter = require('./routes/book')
 const logger = require('./middleware/logger')
 const cookieParser = require('cookie-parser')
 const port = 3000
+const errorHandler = require('./middleware/errorMiddleware')
 
 app.use(logger)
 
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use('/auth', authRouter)
 app.use('/book', bookRouter)
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`listen in ${port}`)
